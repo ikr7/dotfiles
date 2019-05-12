e05b14708f1e2bcbabfc66bc77c0a64f3f530abc
ikr7 <kunpei.ikuta@gmail.com>
2019-05-13 01:48:53 +0900

    add desktop entries

--- settle.sh
+++ settle.sh
@@ -10,3 +10,8 @@ ln -s $PWD/nano ~/.config/nano
 ln -s $PWD/zsh ~/.config/zsh
 
 ln -s $PWD/scripts ~/.scripts
+
+ln -s $PWD/desktop-entries/reboot.desktop ~/.local/share/applications/reboot.desktop
+ln -s $PWD/desktop-entries/screenshot.desktop ~/.local/share/applications/screenshot.desktop
+ln -s $PWD/desktop-entries/shutdown.desktop ~/.local/share/applications/shutdown.desktop
+ln -s $PWD/desktop-entries/suspend.desktop ~/.local/share/applications/suspend.desktop
