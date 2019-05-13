61f73190a2db40a94ad371bbea5628e6aa697eaf
ikr7 <kunpei.ikuta@gmail.com>
2019-05-13 14:29:13 +0900

    split hardware-specific i3 configs

--- settle.sh
+++ settle.sh
@@ -4,7 +4,12 @@ fi
 
 ln -s $PWD/dunst ~/.config/dunst
 ln -s $PWD/fontconfig ~/.config/fontconfig
-ln -s $PWD/i3 ~/.config/i3
+
+mkdir -p $PWD/builds/i3
+cat "$PWD/i3/config-common" > $PWD/builds/i3/config
+cat "$PWD/i3/envs/config-$HOSTNAME" >> $PWD/builds/i3/config
+ln -s "$PWD/builds/i3" ~/.config/i3
+
 ln -s $PWD/i3blocks ~/.config/i3blocks
 ln -s $PWD/nano ~/.config/nano
 ln -s $PWD/zsh ~/.config/zsh
