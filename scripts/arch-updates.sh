27953366993b1014f999cf7b7a5eb0dd9ba26bae
ikr7 <kunpei.ikuta@gmail.com>
2025-05-07 21:12:15 +0900

    update scripts

--- scripts/arch-updates.sh
+++ scripts/arch-updates.sh
@@ -2,11 +2,11 @@ updates=$(checkupdates)
 count=$(echo "$updates" | wc -l)
 
 if [ "$updates" = "" ]; then
-    echo "System up to date"
-    echo "System up to date"
-    echo "#008000"
+    echo "up to date"
+    echo "up to date"
+    # echo "#008000"
 else
-    echo "$count updates available"
-    echo "$count updates available"
-    echo "#FFFF00"
+    echo "$count updates"
+    echo "$count updates"
+    # echo "#FFFF00"
 fi
--- scripts/ip.sh
+++ scripts/ip.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 addr=$(ip -4 addr show dev $INTERFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-echo "Ethernet: $addr"
+echo "$addr"
